ae1307b9cb7823b13553711f8d9afe5b00295c35
Andrei Kirillovykh <andykirill@gmail.com>
2021-04-12 09:46:04 +0300

    1. Авторизация через JDBC[2094#453857]

--- src/main/java/ru/job4j/accident/SecurityConfig.java
+++ src/main/java/ru/job4j/accident/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,10 +16,10 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    DataSource ds;
+    private DataSource ds;
 
 
     @Override
